367601a57536023c871fc6431e2e53ee622ab73c
Sara Schoonover <Saramschoon@gmail.com>
2023-08-03 15:10:56 +0000

    created array to loop through symbols

--- app.rb
+++ app.rb
@@ -1,8 +1,14 @@
 require "sinatra"
 require "sinatra/reloader"
+require "http"
+require "json"
 
 get("/") do
+  symbols_url = "https://api.exchangerate.host/symbols"
+  symbols_data = HTTP.get(symbols_url)
+  parsed_symbols = JSON.parse(symbols_data)
+  @symbols = parsed_symbols.fetch("symbols")
+
+  
   erb(:root)
 end
-
-
