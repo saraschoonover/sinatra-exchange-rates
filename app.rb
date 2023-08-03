92d7b2f8f9c86b07367b2c425c0622792c8702b1
Sara Schoonover <Saramschoon@gmail.com>
2023-08-03 16:01:22 +0000

    added convert symbols page

--- app.rb
+++ app.rb
@@ -8,7 +8,13 @@ get("/") do
   symbols_data = HTTP.get(symbols_url)
   parsed_symbols = JSON.parse(symbols_data)
   @symbols = parsed_symbols.fetch("symbols")
-
+  @only_symbols = @symbols[0]
   
   erb(:root)
+end
+
+get("/:symbol") do 
+  @get_symbols = params.fetch("symbol")
+  
+  erb(:symbols)
 end
